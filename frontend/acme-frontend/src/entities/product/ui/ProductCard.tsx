75cce5ec63db36fd287b6de70646ceebd9449d5f
jeiPa22 <jromerop99@gmail.com>
2025-10-05 05:57:56 -0500

    feat: add ProductCard component to display product details

---
+++ frontend/acme-frontend/src/entities/product/ui/ProductCard.tsx
@@ -0,0 +1,29 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import type { ProductResource } from "../model/types";
+
+/**
+ * Componente para mostrar un producto
+ * @param param0 Props del componente
+ * @returns Componente de tarjeta de producto
+ */
+export function ProductCard({ item }: { item: ProductResource }) {
+  /**
+   * Atributos del producto
+   */
+  const a = item.attributes;
+  
+  /**
+   * Renderizado del componente
+   */
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography variant="subtitle1">{a.name}</Typography>
+        <Typography variant="body2">
+          {a.currency} {a.price.toLocaleString()}
+        </Typography>
+        <Typography variant="caption">Estado: {a.status}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
