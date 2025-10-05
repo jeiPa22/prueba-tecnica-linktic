0bbbd70c84f65915385c0a1a51870c8bf1fbf933
jeiPa22 <jromerop99@gmail.com>
2025-10-05 05:56:22 -0500

    feat: wrap App component with Redux and theme providers

--- frontend/acme-frontend/src/main.tsx
+++ frontend/acme-frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { QueryProvider } from "./app/providers/QueryClient.tsx";
+import { AppThemeProvider } from "./app/providers/Theme.tsx";
+import { store } from "./app/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <QueryProvider>
+        <AppThemeProvider>
+          <App />
+        </AppThemeProvider>
+      </QueryProvider>
+    </Provider>
+  </StrictMode>
+);
