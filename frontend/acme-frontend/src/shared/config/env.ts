17e5e35b4ab9aad6ea7934bc3c09d43862d9fa00
jeiPa22 <jromerop99@gmail.com>
2025-10-05 07:47:23 -0500

    fix: api key env

--- frontend/acme-frontend/src/shared/config/env.ts
+++ frontend/acme-frontend/src/shared/config/env.ts
@@ -1,6 +1,7 @@
 export const env = {
   PRODUCTS_URL: import.meta.env.VITE_PRODUCTS_URL as string,
+  PRODUCTS_API_KEY: import.meta.env.VITE_PRODUCTS_API_KEY as string,
   INVENTORY_URL: import.meta.env.VITE_INVENTORY_URL as string,
+  INVENTORY_API_KEY: import.meta.env.VITE_INVENTORY_API_KEY as string,
   API_KEY_HEADER: import.meta.env.VITE_API_KEY_HEADER as string,
-  API_KEY: import.meta.env.VITE_API_KEY as string,
 };
