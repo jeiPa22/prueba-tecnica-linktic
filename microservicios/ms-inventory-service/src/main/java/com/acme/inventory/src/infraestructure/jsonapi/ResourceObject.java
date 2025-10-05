05481e3cfe231b40f95ddb350b5f9a24c4ca77a2
jeiPa22 <jromerop99@gmail.com>
2025-10-04 23:03:51 -0500

    infraestructure: jsonapi, openapi

---
+++ microservicios/ms-inventory-service/src/main/java/com/acme/inventory/src/infraestructure/jsonapi/ResourceObject.java
@@ -0,0 +1,26 @@
+package com.acme.inventory.src.infraestructure.jsonapi;
+
+import java.util.Map;
+
+public record ResourceObject(
+        /**
+         * el tipo de recurso
+         */
+        String type,
+        /**
+         * el id del recurso
+         */
+        String id,
+        /**
+         * los atributos del recurso
+         */
+        Map<String, Object> attributes,
+        /**
+         * las relaciones del recurso
+         */
+        Map<String, Object> relationships,
+        /**
+         * los enlaces del recurso
+         */
+        Map<String, String> links) {
+}
